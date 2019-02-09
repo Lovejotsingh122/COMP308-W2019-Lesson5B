4490e6123c5815037fae08f26f4387b7d333916c
lovejotsingh122 <lovejotsangha122@gmail.com>
2019-02-09 16:10:33 -0500

    done with  adding an contact

--- routes/contact.js
+++ routes/contact.js
@@ -22,32 +22,77 @@ router.get('/', (req, res, next) =>{
         }
     });
 });
-//get route for the add page 
-// this will display the add page
-router.get('/add',(req,res,next)=>{
-    res.render('contacts/add',{
-        title:'Add New Contact'
+
+/* GET Route for the Add page 
+   this will display the Add page */
+router.get('/add', (req, res, next) => {
+    res.render('contacts/add', {
+        title: 'Add New Contact'
     });
 });
 
+/* POST Route for processing the Add page */
+router.post('/add', (req, res, next) => {
 
+    let newContact = contactModel({
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName,
+        "age": req.body.age
+    });
 
-router.post('/add',(req,res,next)=>{
-let newContact=contctModel({
-    "firstName":req.body.firstName,
-    "lastName":req.body.lastName,
-    "age":req.body.age
-
+    contactModel.create(newContact, (err, contactModel) => {
+        if(err) {
+            console.log(err);
+            res.end(err);
+        }
+        else {
+            // refresh the contact list
+            res.redirect('/contact-list');
+        }
+    });
 });
 
-contactModel.create(newContact,(err,contactModel)=>{
-    if(err){
-        console.log(err);
-        res.end(err);
-    }
-    else{ 
-         res.redirect('/contactList');
-    }
+/* GET request - display the Edit page */
+router.get('/edit/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    contactModel.findById(id, (err, contactObject) => {
+        if(err) {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            // show the edit view
+            res.render('contacts/edit', {
+                title: 'Edit Contact',
+                contact: contactObject
+            });
+        }
+    });
 });
+
+/* POST request - Update the database with data from the Edit Page */
+router.post('/edit/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    let updatedContact = contactModel({
+        "_id": id,
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName,
+        "age": req.body.age
+    });
+
+    contactModel.update({_id: id}, updatedContact, (err) => {
+        if(err) {
+            console.log(err);
+            res.end(err);
+        }
+        else {
+            // refresh the contact list
+            res.redirect('/contact-list');
+        }
+    })
 });
+
 module.exports = router;
